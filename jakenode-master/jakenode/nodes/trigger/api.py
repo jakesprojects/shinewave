131822359f86e07f7a0e3f997bb1b45fb0fb6285
Jake <millerjacobr@gmail.com>
2023-02-04 11:24:15 -0800

    Added customized node library, main version

---
+++ jakenode-master/jakenode/nodes/trigger/api.py
@@ -0,0 +1,19 @@
+from jakenode.nodes.trigger.trigger_node import TriggerNode
+
+
+class TriggerAPI(TriggerNode):
+    """
+    A node triggered by an API call.
+    """
+
+    # unique node identifier.
+    __identifier__ = 'nodes.trigger'
+
+    # initial default node name.
+    NODE_NAME = 'API Trigger'
+
+    def __init__(self):
+        super(TriggerAPI, self).__init__()
+
+        # create node outputs.
+        # self.add_output('output_1')
