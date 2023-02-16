a23ea447d3f38783fb39d21ea6d07a68e3f160de
Jake <millerjacobr@gmail.com>
2023-02-15 17:08:40 -0800

    Moved special pages out of generic handler function

--- file_mount/frontend/apps/home/routes.py
+++ file_mount/frontend/apps/home/routes.py
@@ -17,54 +17,14 @@ DATABASE = '/srv/node_app/handlers/data/test_db.db'
 @blueprint.route('/index')
 @login_required
 def index():
-
     return render_template('home/index.html', segment='index')
 
 
-@blueprint.route('/<template>')
+@blueprint.route('/workflow-builder.html', methods=['GET'])
+@blueprint.route('/workflow-builder', methods=['GET'])
 @login_required
-def route_template(template):
-
-    try:
-
-        if template.endswith('.html'):
-            template_name = template.rsplit('.html', 1)[0]
-        else:
-            template_name = template
-            template += '.html'
-
-        special_formatting = handle_special_template(template_name)
-
-        # Detect the current page
-        segment = get_segment(request)
-
-        # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment, **special_formatting)
-
-    except TemplateNotFound:
-        return render_template('home/page-404.html'), 404
-
-    except:
-        return render_template('home/page-500.html'), 500
-
-
-# Helper - Extract current page name from request
-def get_segment(request):
-
-    try:
-
-        segment = request.path.split('/')[-1]
-
-        if segment == '':
-            segment = 'index'
-
-        return segment
-
-    except:
-        return None
-
-
-def handle_special_template(template_name):
+def workflow_builder():
+    # request.args
     def _workflow_tree_dict_to_json(workflow_tree_dict):
         json_list = [
             {'text': 'New Folder', 'icon': 'jstree-ok'}
@@ -97,43 +57,89 @@ def handle_special_template(template_name):
 
         return json.dumps(json_list)
 
-    if template_name == 'workflow-builder':
+    tree_format = {}
+    with sqlite3.connect(DATABASE) as conn:
+        cur = conn.cursor()
+        cur.execute(f"""
+            SELECT
+                wc.name AS workflow_category,
+                w.name,
+                w.id
+            FROM workflow_categories wc
+            LEFT JOIN workflows w ON
+                wc.id=w.workflow_category_id
+                AND wc.active=w.active
+            WHERE
+                wc.account_id={ACCOUNT_ID}
+                AND wc.active='TRUE'
+            ORDER BY wc.name, w.name
+        """)
+        workflow_data = cur.fetchall()
 
-        tree_format = {}
-        with sqlite3.connect(DATABASE) as conn:
-            cur = conn.cursor()
-            cur.execute(f"""
-                SELECT
-                    wc.name AS workflow_category,
-                    w.name,
-                    w.id
-                FROM workflow_categories wc
-                LEFT JOIN workflows w ON
-                    wc.id=w.workflow_category_id
-                    AND wc.active=w.active
-                WHERE
-                    wc.account_id={ACCOUNT_ID}
-                    AND wc.active='TRUE'
-                ORDER BY wc.name, w.name
-            """)
-            workflow_data = cur.fetchall()
-        for workflow_category, workflow_name, workflow_id in workflow_data:
-            tree_format.setdefault(workflow_category, [])
-            tree_format[workflow_category].append((workflow_name, workflow_id))
+    for workflow_category, workflow_name, workflow_id in workflow_data:
+        tree_format.setdefault(workflow_category, [])
+        tree_format[workflow_category].append((workflow_name, workflow_id))
 
-        tree_format_code = _workflow_tree_dict_to_json(tree_format)
+    tree_format_code = _workflow_tree_dict_to_json(tree_format)
 
-        # Not sure how this is used
-        tree_format_text = """<!-- 
-            <li data-jstree='{ "opened" : true }'>Root node
-                <ul>
-                    <li data-jstree='{ "selected" : true }'>Child node 1</li>
-                    <li>Child node 2</li>
-                </ul>
-            </li>
-        -->
-        """
+    # Not sure how this is used
+    tree_format_text = """<!-- 
+        <li data-jstree='{ "opened" : true }'>Root node
+            <ul>
+                <li data-jstree='{ "selected" : true }'>Child node 1</li>
+                <li>Child node 2</li>
+            </ul>
+        </li>
+    -->
+    """
 
-        return {'tree_format_text': tree_format_text, 'tree_format_code': tree_format_code}
-    else:
-        return {}
+    return render_template(
+        'home/workflow-builder.html', tree_format_text=tree_format_text, tree_format_code=tree_format_code
+    )
+
+
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+
+    try:
+
+        if template.endswith('.html'):
+            template_name = template.rsplit('.html', 1)[0]
+        else:
+            template_name = template
+            template += '.html'
+
+        # special_formatting = handle_special_template(template_name)
+        special_formatting = {}
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        import sys
+        print(f'~SEGMENT~\n{segment}', file=sys.stderr)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment, **special_formatting)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
