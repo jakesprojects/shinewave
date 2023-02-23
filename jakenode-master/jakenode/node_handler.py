4e623af1c17f1258a6ac92ec60a24a514caad651
Jake <millerjacobr@gmail.com>
2023-02-22 23:03:41 -0800

    Changed AtTimeTrigger font color to black

--- jakenode-master/jakenode/node_handler.py
+++ jakenode-master/jakenode/node_handler.py
@@ -15,6 +15,6 @@ def fetch_node_display_info(node):
 
 def fetch_node_text_color(node_type):
 	default_color = '#FFFFFF' # White
-	node_color_dict = {'nodes.trigger.TimeElapsedTrigger': '#000000'}
+	node_color_dict = {'nodes.trigger.TimeElapsedTrigger': '#000000', 'nodes.trigger.AtTimeTrigger': '#000000'}
 	return node_color_dict.get(node_type, default_color)
 	
