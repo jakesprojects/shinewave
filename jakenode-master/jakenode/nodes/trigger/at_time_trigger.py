1b1c88c65586bd6927a550bbefcb11dfa6b28005
Jake <millerjacobr@gmail.com>
2023-02-22 23:35:58 -0800

    Added node validation logic, added to-do list, added ability to turn text into datetime

--- jakenode-master/jakenode/nodes/trigger/at_time_trigger.py
+++ jakenode-master/jakenode/nodes/trigger/at_time_trigger.py
@@ -1,8 +1,16 @@
+from collections import OrderedDict
+from datetime import datetime
 import html
 import re
 
 from jakenode.nodes.trigger.trigger_node import TriggerNode
 
+
+"""
+TO DO:
+    * validate that dates are in future
+    * add timezone support (Maybe via dropdown?)
+"""
 
 class AtTimeTrigger(TriggerNode):
     """
@@ -21,52 +29,65 @@ class AtTimeTrigger(TriggerNode):
 
         self.set_property('color', (215, 254, 203))
 
-    # def get_display_info(self, node_templates_root=None):
-    #     """
-    #         Broadcasts the info to be displayed in the display_window. Output should be a tuple composed of:
-    #             1. The Node's Name
-    #             2. An HTML block to display
-    #     """
+    def get_display_info(self, node_templates_root=None):
+        """
+            Broadcasts the info to be displayed in the display_window. Output should be a tuple composed of:
+                1. The Node's Name
+                2. An HTML block to display
+        """
 
-    #     node_name = self.get_node_name(html_safe=True)
+        node_name = self.get_node_name(html_safe=True)
 
-    #     try:
-    #         time_number = self.get_time_number()
-    #     except ValueError as exception:
-    #         return node_name, f'<p style="background-color:red;">{exception}</p>'
+        try:
+            execution_date = self.get_execution_date()
+        except ValueError as exception:
+            return node_name, f'<p style="background-color:red;">{exception}</p>'
+
+        display_text = f'<p>The next step in the workflow will be triggered at {execution_date} without any other'
+        display_text += ' downstream event occurring first.</p>'
+
+        return node_name, display_text
+
+    def get_execution_date(self):
+
+        format_dict = OrderedDict(
+            {
+                '%Y/%m/%d %H:%M': 'YYYY/MM/DD HOUR:MINUTE',
+                '%Y-%m-%d %H:%M': 'YYYY-MM-DD HOUR:MINUTE',
+                '%Y/%m/%d': 'YYYY/MM/DD',
+                '%Y-%m-%d': 'YYYY-MM-DD'
+            }
+        )
+
+        execution_date_text = self.get_property('execution_date').strip()
+        execution_date_text = html.escape(execution_date_text)
+
+        if execution_date == '':
+            raise ValueError('"Execution Date/Time" is blank.')
         
-    #     time_units = self.get_property('time_units')
+        error_msg = ''
 
-    #     if time_number == 1:
-    #         time_units_text = f'{time_number} {time_units[:-1]} passes'
-    #     else:
-    #         time_units_text = f'{time_number} {time_units} pass'
+        python_format_list = list(format_dict.copy().keys())
+        execution_date = None
 
-    #     display_text = f'<p>The next step in the workflow will be triggered for all audience members attached to the workflow {time_units_text} without any other'
-    #     display_text += ' downstream event occurring.</p>'
+        while execution_date is None and python_format_list:
+            python_format = python_format_list.pop(0)
+            try:
+                execution_date = datetime.strptime(execution_date_text, python_format)
+            except ValueError:
+                execution_date = None
+                error_msg += f'<br>    {format_dict[python_format]}'
 
-    #     return node_name, display_text
+        if execution_date is None:
+            error_msg = '"Execution Date/Time" must be provided in one of the following formats:' + error_msg
+            error_msg += f'<br>You have entered "{execution_date_text}"'
+            raise ValueError(error_msg)
 
-    # def get_time_number(self):
-    #     time_number = self.get_property('time_number').strip()
-    #     time_number = html.escape(time_number)
+        return execution_date
 
-    #     if time_number == '':
-    #         raise ValueError('"Number of Time Units" is blank.')
-    #     elif re.sub('[0-9]', '', time_number):
-    #         raise ValueError(
-    #             f'"Number of Time Units" must be a positive, whole number. You have entered "{time_number}".'
-    #         )
-    #     elif time_number.startswith('0'):
-    #         raise ValueError(
-    #             f'"Number of Time Units" must not be zero or start with zero. You have entered "{time_number}".'
-    #         )
+    def validate_node(self):
+        """
+            Raises a ValueError if an invalid value has been entered in the "Number of Time Units" free-text field
+        """
 
-    #     return int(time_number)
-
-    # def validate_node(self):
-    #     """
-    #         Raises a ValueError if an invalid value has been entered in the "Number of Time Units" free-text field
-    #     """
-
-    #     self.get_time_number()
+        self.get_execution_date()
