8451db47b97a5fc6f38270ed27544b4a4bd4b87c
jakesprojects <millerjacobr@gmail.com>
2023-03-01 21:09:56 -0800

    Port assignment now handled elsewhere

--- file_mount/node_app/run_two_window_app.py
+++ file_mount/node_app/run_two_window_app.py
@@ -17,7 +17,7 @@ parser.add_argument(
 )
 parser.add_argument("-wid", "--workflow-id", dest="workflow_id", default="1", help="Workflow ID")
 # parser.add_argument("-xp", "xpra-port", dest="xpra_port", default="49153", help="XPRA Port")
-# parser.add_argument("-ipp", "info-panel-port", dest="info_panel_port", default="49153", help="Info Panel Port")
+parser.add_argument("-ipp", "--info-panel-port", dest="info_panel_port", default="49153", help="Info Panel Port")
 # parser.add_argument("-ws", "websocket", dest="websocket", default="49153", help="Websocket")
 # parser.add_argument("-xd", "x11-display", dest="x11_display", default="49153", help="X11 Display")
 
@@ -26,7 +26,7 @@ account_id = int(args.account_id)
 workflow_category_id = int(args.workflow_category_id)
 workflow_id = int(args.workflow_id)
 # xpra_port = int(args.xpra_port)
-# info_panel_port = int(args.info_panel_port)
+info_panel_port = int(args.info_panel_port)
 # websocket = int(args.websocket)
 # x11_display = int(args.x11_display)
 
@@ -40,111 +40,14 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
-# Functions to handle port assignment
-def invert_address_types(address_types):
-    column_types = {}
-    for address_type, column_list in address_types.items():
-        for column_name in column_list:
-            column_types[column_name] = address_type
-    return column_types
-
-
-def generate_fetch_query(columns, destination_server):
-    address_select_clause = f",\n            ".join(columns)
-
-    return f"""
-        SELECT
-            {address_select_clause}
-        FROM workflow_routes
-        WHERE
-            app_server_address='{destination_server}'
-    """
-
-
-def fetch_taken_addresses(column_types, destination_server):
-            
-    query = generate_fetch_query(column_types.keys(), destination_server)
-    
-    taken_addresses = run_query(query, return_data_format=dict)
-    filtered_taken_addresses = {}
-    for column_name, address_type in column_types.items():
-        filtered_taken_addresses.setdefault(address_type, [])
-        filtered_taken_addresses[address_type] += [i for i in taken_addresses[column_name] if isinstance(i, int)]
-
-    return filtered_taken_addresses
-
-
-def assign_next_port(workflow_id):
-    DESTINATION_SERVER = 'localhost'
-    
-    address_search_ranges = {
-        'ports': (49152, 65535),
-        'websockets': (49152, 65535),
-        'x11_displays': (0, 99)
-    }
-    
-    address_types = {
-        'ports': ['xpra_port', 'info_panel_port'],
-        'websockets': ['websocket'],
-        'x11_displays': ['x11_display']
-    }
-    
-    column_types = invert_address_types(address_types)
-
-    workflow_addresses = run_query(
-        f"""
-            {generate_fetch_query(chain(*address_types.values()), DESTINATION_SERVER)}
-            AND workflow_id={workflow_id}
-        """,
-        return_data_format=dict
-    )
-    
-    taken_address_dict = fetch_taken_addresses(column_types, DESTINATION_SERVER)
-    update_column_dict = {}
-
-    for column_name in column_types:
-        if not workflow_addresses[column_name][0]:
-            address_type = column_types[column_name]
-            taken_addresses = taken_address_dict[address_type]
-            current_address, max_allowable_address = address_search_ranges[address_type]
-            assigned_address = None
-            
-            max_value_err_msg = f'Cannot assign {address_type}.'
-            max_value_err_msg += f'All values {current_address}-{max_allowable_address} already taken.'
-            
-            while assigned_address is None:
-                if current_address > max_allowable_address:
-                    raise Exception(max_value_err_msg)
-                elif current_address not in taken_addresses:
-                    assigned_address = current_address
-                else:
-                    current_address += 1
-            update_column_dict[column_name] = assigned_address
-            
-
-    if update_column_dict:
-        set_clause = []
-        for column_name, set_value in update_column_dict.items():
-            set_clause.append(f'{column_name}={set_value}')
-        set_clause = ',\n            '.join(set_clause)
-        update_statement = f"""
-            UPDATE workflow_routes
-            SET
-                {set_clause}
-            ;
-        """
-        print(update_statement)
-        run_query(update_statement, commit=True)
-
-
 def run_two_window_app(
     socketio=socketio,
     thread_lock=thread_lock,
     account_id=account_id,
     workflow_category_id=workflow_category_id,
-    workflow_id=workflow_id
+    workflow_id=workflow_id,
+    info_panel_port=info_panel_port
 ):
-
 
     def background_thread(
         socketio=socketio, account_id=account_id, workflow_category_id=workflow_category_id, workflow_id=workflow_id
@@ -182,7 +85,12 @@ def run_two_window_app(
                 thread = socketio.start_background_task(background_thread)
         emit('my_response', {'data': 'Connected', 'count': 0})
 
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=info_panel_port,
+        allow_unsafe_werkzeug=True
+    )
 
 if __name__ == '__main__':
     run_two_window_app()
