7707918be0a06ab66b50d5aa9f944a0adbb73f52
jakesprojects <millerjacobr@gmail.com>
2023-04-09 14:48:34 -0700

    Added module for storing regex patterns and functions

---
+++ jakenode-master/jakenode/regex.py
@@ -0,0 +1,20 @@
+import re
+
+class regex_templates():
+
+    def __init__(self):
+        self.templates_dict = {
+            'Yes': {'template': r'\by(es)?\b', 'examples': ['Yes', 'y']},
+            'No': {'template': r'\bn(o)?\b', 'examples': ['No', 'n']},
+            'Sí': {'template': r'\bs(í|i)\b', 'examples': ['Si', 'sí']}
+        }
+
+    def get_all_templates(self):
+        return list(self.templates_dict)
+
+    def get_examples(self, template_name):
+        return self.templates_dict[template_name]['examples']
+
+    def get_template(self, template_name):
+        template = self.templates_dict[template_name]['template']
+        return re.compile(return_info, re.IGNORECASE)
